7144ea8e35aadd1d14035c329b18871ac6246549
Gyeop <yanggieop@gmail.com>
2022-04-06 09:11:48 +0900

    Add day5 files

---
+++ rescript/src/Week1/Year2020Day5.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+const sample = fs.readFileSync("input/Week1/Year2020Day5.sample.txt", "utf8");
+
+console.log(sample);
---
+++ rescript/src/Week1/Year2020Day5.res
@@ -0,0 +1,1 @@
+let input = Node.Fs.readFileAsUtf8Sync("input/Week1/Year2020Day5.sample.txt")
